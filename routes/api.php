a25c216fa67d8bda55d5d9ffac088d26bf22514a
codetesla51 <uoladele99@gmail.com>
2025-03-10 04:25:50 +0100

    backend update

--- routes/api.php
+++ routes/api.php
@@ -11,19 +11,22 @@ Route::get("/projects/{slug}", [ProjectController::class, "show"]);
 
 // Protected routes for projects (POST, PUT, DELETE)
 Route::middleware([AdminAuthMiddleware::class])->group(function () {
-    Route::post("/projects", [ProjectController::class, "store"]);
-    Route::put("/projects/{slug}", [ProjectController::class, "update"]);
-    Route::delete("/projects/{slug}", [ProjectController::class, "destroy"]);
+  Route::post("/projects", [ProjectController::class, "store"]);
+  Route::put("/projects/{slug}", [ProjectController::class, "update"]);
+  Route::delete("/projects/{slug}", [ProjectController::class, "destroy"]);
 });
 
 // Contact routes
 Route::post("/contacts", [ContactController::class, "store"]); // Public
 Route::middleware([AdminAuthMiddleware::class])->group(function () {
-    Route::get("/contacts", [ContactController::class, "index"]);
-    Route::get("/contacts/{id}", [ContactController::class, "show"]);
-    Route::delete("/contacts/{id}", [ContactController::class, "destroy"]);
+  Route::get("/contacts", [ContactController::class, "index"]);
+  Route::get("/contacts/{id}", [ContactController::class, "show"]);
+  Route::put("/contacts/{id}", [ContactController::class, "update"]); // New route for marking as read
+  Route::delete("/contacts/{id}", [ContactController::class, "destroy"]);
 });
 
 // Admin authentication routes
 Route::post("/admin/login", [AdminController::class, "login"]);
-Route::post("/admin/logout", [AdminController::class, "logout"])->middleware(AdminAuthMiddleware::class);
+Route::post("/admin/logout", [AdminController::class, "logout"])->middleware(
+  AdminAuthMiddleware::class
+);
